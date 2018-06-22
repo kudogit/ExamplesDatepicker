ae907bb84c2abcac109051a631c7303d275b4fbe
KhoiSTS <khoiaesgit@gmail.com>
2018-06-23 03:48:00 +0700

    fixed

--- src/app/datepicker.component.ts
+++ src/app/datepicker.component.ts
@@ -13,19 +13,24 @@ declare var $: any;
 
 @Component({
   selector: 'datepicker',
-  templateUrl:'./datepicker.component.html'
+  templateUrl: './datepicker.component.html'
 })
 
 
-export class DatepickerComponent implements  AfterViewInit {
+export class DatepickerComponent implements AfterViewInit {
   date = moment(new Date().getDate()).format('DD-MM-YY');
+  availableDates = [
+    "2018-06-23","2018-06-26","2018-06-28",
+    "2018-06-11","2018-06-12","2018-06-16",
+    "2018-06-5","2018-06-2","2018-06-3",
+  ]
   @Output()
   changeCurrenFilter = new EventEmitter<any>();
 
   @Output()
   change: EventEmitter<string> = new EventEmitter();
 
-  @Input() currenFilter:any;
+  @Input() currenFilter: any;
 
   options = {
     minDate: new Date(),
@@ -33,8 +38,8 @@ export class DatepickerComponent implements  AfterViewInit {
   }
   constructor(private elementRef: ElementRef) {
   }
-  
-  increment(){
+
+  increment() {
     this.changeCurrenFilter.emit(this.currenFilter);
   }
 
@@ -44,36 +49,47 @@ export class DatepickerComponent implements  AfterViewInit {
         changeMonth: true,
         changeYear: true,
         showButtonPanel: true,
-        dateFormat:'MM',
+        dateFormat: 'MM',
         onClose: function (dateText, inst) {
           var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
           var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-          $(this).datepicker('setDate',new Date(year, month, 1));
+          $(this).datepicker('setDate', new Date(year, month, 1));
         }
       });
       $(".datepicker_mouthly").focus(function () {
         $(".ui-datepicker-year").hide();
         $(".ui-datepicker-calendar").hide();
       });
-    } 
+    }
     else if (this.currenFilter == 'year') {
-      $(".datepicker_year").datepicker( {
+      $(".datepicker_year").datepicker({
         changeMonth: true,
         changeYear: true,
         showButtonPanel: true,
         dateFormat: 'yy',
         onSelect: (dateText) => {
           this.date = dateText;
-       },
-        onClose: function(dateText, inst) { 
-            //var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-            $(this).datepicker('setDate',new Date(year));
+        },
+        onClose: function (dateText, inst) {
+          //var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+          var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+          $(this).datepicker('setDate', new Date(year));
         }
-    });
+      });
       $(".datepicker_year").focus(function () {
         $(".ui-datepicker-calendar").hide();
         $(".ui-datepicker-month").hide();
+      });
+    }
+    else if (this.currenFilter == 'random') {
+      var array = this.availableDates;
+      $(".datepicker_random").datepicker({
+        dateFormat: 'dd-mm-yy',
+        beforeShowDay: function(date){
+          var string = $.datepicker.formatDate('yy-mm-dd', date);
+          return [ array.indexOf(string) != -1 ]
+      }
+  
       });
     }
     else {
@@ -82,9 +98,9 @@ export class DatepickerComponent implements  AfterViewInit {
         maxDate: this.options.maxDate,
         showButtonPanel: true,
         onSelect: (dateText) => {
-           this.date = dateText;
+          this.date = dateText;
         },
-        
+        calendarWeeks: true,
         beforeShow: function (input) {
           setTimeout(function () {
             var buttonPane = $(input)
